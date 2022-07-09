e1c04eb62cb2537f00c5402451381c6c0338671f
Yanina063 <strrrekosa@mail.ru>
2022-07-09 20:54:24 +0400

    решили задачу

--- Kontrolnaya_1/Program.cs
+++ Kontrolnaya_1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Из имеющегося массива сформировать массив строк, длинна которых меньше или равна 3 символам
+string [] array = { "1234", "1567", "-2", "computer science" };
+
+string [] result = array.Where(item => item.Length <= 3).ToArray();
+
+Console.WriteLine("Результат: ");
+
+
+foreach(string element in result) {
+    Console.WriteLine(element);
+}
+
 
